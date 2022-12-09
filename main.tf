a17f2e3a7973a490715f5ab40c7686025980ff8a
ExeQutionn <vladimir980@outlook.com>
2022-12-09 10:10:09 +0100

    more fixes

--- main.tf
+++ main.tf
@@ -1,50 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket = "s3-bucket-state"
-    key = "global/s3/terraform.tfstate"
-    region = "eu-west-3"
-    encrypt = true
-  }
-}
-
-
-provider "aws" {
-  region = "eu-west-3"
-}
-//TODO: add s3 as a backend
-//investigate how to revert broken state file on s3 bucket
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "s3-bucket-state"
-  lifecycle {
-    prevent_destroy = true
-  }
-  
-  
-}
-resource "aws_s3_bucket_versioning" "enabled" {
-  bucket = aws_s3_bucket.terraform_state.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-    
-}
-resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
-  bucket = aws_s3_bucket.terraform_state.id
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
-resource "aws_s3_bucket_public_access_block" "public_access" {
-  bucket = aws_s3_bucket.terraform_state.id
-  block_public_acls = true
-  block_public_policy = true
-  ignore_public_acls = true
-  restrict_public_buckets = true
-}
-
-
 //TODO: rename VPC
 resource "aws_vpc" "nginx_vpc" {
   cidr_block = "10.0.0.0/16"
@@ -73,6 +26,8 @@ resource "aws_lb_target_group" "group" {
   vpc_id = aws_vpc.nginx_vpc.id
 
 }
+
+
 resource "aws_lb_listener" "listener_http" {
   load_balancer_arn = aws_lb.app_lb.arn
   port = "80"
--- sg.tf
+++ sg.tf
@@ -51,37 +51,3 @@ resource "aws_security_group" "sg_ec2" {
     "Name" = "EC2 Security group"
   }
 }
-
-resource "aws_default_network_acl" "nacl" {
-  default_network_acl_id = aws_vpc.nginx_vpc.default_network_acl_id
-
-  egress {
-    protocol   = "tcp"
-    rule_no    = 200
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 443
-    to_port    = 443
-  }
-
-  ingress {
-    protocol   = "tcp"
-    rule_no    = 100
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 80
-    to_port    = 80
-  }
-  ingress {
-    protocol   = "tcp"
-    rule_no    = 200
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 22
-    to_port    = 22
-  }
-
-  tags = {
-    Name = "Premium Auto Nacl"
-  }
-}
